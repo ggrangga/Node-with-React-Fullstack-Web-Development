69232fabdb397b102920a8999da57b6e74f22eb1
ggrangga@bitbucket.org <ggrangga@bitbucket.org>
2018-09-13 10:04:48 +0700

    BodyParser Middleware

--- server/config/dev.js
+++ server/config/dev.js
@@ -2,5 +2,7 @@ module.exports = {
 	googleClientID: '765613067636-4j1rm97bmeuk1imfb83bo3jtfq0bjbgl.apps.googleusercontent.com',
 	googleClientSecret: 'LU1owio1mpedP_1zg-dUBfBl',
 	mongoURI: 'mongodb://root:dbroot9@ds113732.mlab.com:13732/emaily-dev',
-	cookieKey: 'zdgvdsgfsgfdsgsdfhsertrsegrsegrse'
+	cookieKey: 'zdgvdsgfsgfdsgsdfhsertrsegrsegrse',
+	stripePublishableKey: 'pk_test_8jXEOMUjfhvmhPeAkBus5IuX',
+	stripeSecretKey: 'sk_test_96m3yCKfXBO1bbPc9rxb04lS'
 };
